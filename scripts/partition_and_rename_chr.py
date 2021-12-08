d868e1a42d9adba0ed5b6f2677afd84754ff16fc
miriambt <78080849+miriambt@users.noreply.github.com>
2021-12-08 12:54:20 +0100

    Update partition_and_rename_chr.py

--- scripts/partition_and_rename_chr.py
+++ scripts/partition_and_rename_chr.py
@@ -1,6 +1,7 @@
 ################################################################################
-# Reads single .vcf file
-# Renames variants/CHROM nomenclature from '1' to 'chr1',..., '38' to 'chr38'
+# Reads a single .vcf file
+# Filters the data by chromosome
+# Renames variants/CHROM nomenclature from '1' to 'chr1', ..., '38' to 'chr38'
 # Writes the result in separate .vcf files, one for each chromosome
 ################################################################################
 
@@ -9,11 +10,18 @@ sys.path.append('/home/users/miriambt/my_work/dog-gen-to-phen/preprocessing')
 
 from utils.vcf_utils import read_vcf_file, filter_by_chromosome, rename_chromosome, write_vcf_file
 
+################################################################################
+
+## USER INPUTS
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 ## Define base path to .vcf file with data for all chromosomes
 PATH = '/scratch/users/miriambt/data/dogs/formatted_data/4th_dataset/array/All_Pure_150k.vcf'
 
 ## Define base path to output .vcf file with data for a single chromosome
 OUTPUT_PATH = '/scratch/users/miriambt/data/dogs/formatted_data/4th_dataset/array/All_Pure_150k_chr*.vcf'
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Read .vcf file using skikit-allel
 data = read_vcf_file(PATH)
