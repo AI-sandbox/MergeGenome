2a852ea4575ae6c362ef42b78d5ba178a3a1333c
miriambt <78080849+miriambt@users.noreply.github.com>
2021-12-09 16:58:14 +0100

    Update track.py

--- utils/track.py
+++ utils/track.py
@@ -1,37 +1,34 @@
 ################################################################################
-# Tracking in .txt file
+# Tracks the results of a script in a .txt file
+# Similar to print() but saving the results in a .txt file
 ################################################################################
 
 import os
 
-def track(message, track_name):
+def track(message, track_path):
     '''
     Objective:
-        - Write results and tracking of the script in .txt file.
+        - Write tracking message in .txt file stored at track_path. If the .txt file does not exist, it is created.
     Input:
-        - message: message to write in .txt file.
-        - track_name: name of .txt file to write results.
+        - message: message to write at the end of .txt file.
+        - track_path: path to output .txt file that will store the tracking of the script.
     '''
-    
-    ## Define path to .txt file
-    base_path = '/home/users/miriambt/my_work/dog-gen-to-phen/preprocessing/output'
-    file = '{}/{}'.format(base_path, track_name)
-    
-    ## If it does not already exist, create directory that will contain the results and tracking of the script
+       
+    ## If it does not already exist, create directory that will contain the .txt with the tracking of the script
     try:
-        os.makedirs(base_path)
+        os.makedirs(track_path)
     except OSError:
         pass
 
     ## Open the file in append & read mode ('a+')
-    with open(file, "a+") as file_object:
+    with open(track_path, "a+") as file_object:
         ## Move read cursor to the start of the file
         file_object.seek(0)
-        
-        ## If the file is not empty, then append new line '\n'
         data = file_object.read()
         if len(data) > 0 :
+            ## If the file is not empty...
+            # Append new line '\n'
             file_object.write("\n")
         
-        ## Append text at the end of file
+        ## Append text message at the end of the file
         file_object.write(message)
